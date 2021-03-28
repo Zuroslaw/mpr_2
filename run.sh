c4b0227d535d069334e6e45b49ed2b4cd374f3c4
Mateusz Żurowski <mateusz.zurowski@gmail.com>
2021-03-28 21:50:44 +0200

    changes

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 #SBATCH --nodes 1
 #SBATCH --ntasks 12
 #SBATCH --constraint="intel"
-#SBATCH --time=00:02:00
+#SBATCH --time=20:00:00
 #SBATCH --partition=plgrid
 #SBATCH --account=plgmpr21zeus
 
 pwd
 module add plgrid/tools/openmpi
 mpicc -std=c99 -o mc_p mc_p.c
-echo "costam"
-#python3 ./run.py ./mc_p 10
+
+python3 ./run.py ./mc_p 10
